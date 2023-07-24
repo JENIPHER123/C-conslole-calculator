40f3a9ed8530cddad42f35a089d0c4eea6159920
JENIPHER123 <jeniperkuki@gmail.com>
2023-07-24 21:09:35 +0300

    adding body structure

--- Scientific Calculator/Scientific Calculator/Program.cs
+++ Scientific Calculator/Scientific Calculator/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Coding with Jenny!!");
+string input;
+
+while (true)
+{
+    Console.WriteLine("Welcome to Coding with Jenny!!\nScientific Calculator");
+
+    input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+    {
+        Console.WriteLine("Exiting the program...");
+        break; // Exit the while loop
+    }
+    // Place your program logic here (what the program does while running)
+}
